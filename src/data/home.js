7bb3f58520b20c13f005023b7e2ab456cd0327ec
Andrew Stam <j.andrew.stam@gmail.com>
2026-04-06 13:12:18 -0400

    change years of experience in home.js data

--- src/data/home.js
+++ src/data/home.js
@@ -1,7 +1,7 @@
 const HOME = {
   title: 'Software Engineer',
   summary:
-    'Software Engineer with 15+ years of experience designing, building, and modernizing enterprise backend systems. Expert in scalable REST APIs, SQL Server performance tuning, and legacy modernization using Clean Architecture, CQRS, and the Repository Pattern. Comfortable across the full software lifecycle, from design to production support, in Agile environments.',
+    'Software Engineer with 20+ years of experience designing, building, and modernizing enterprise backend systems. Expert in scalable REST APIs, SQL Server performance tuning, and legacy modernization using Clean Architecture, CQRS, and the Repository Pattern. Comfortable across the full software lifecycle, from design to production support, in Agile environments.',
 }
 
 export default HOME
